0ebbd904c091394ef44519d656561e8e750f8017
sakshamjoshi <sakshamjoshi1975@gmail.com>
2024-05-29 17:30:28 +0530

    Taking pull

--- user-module/src/main/java/com/nagarro/usermodule/controller/UserController.java
+++ user-module/src/main/java/com/nagarro/usermodule/controller/UserController.java
@@ -1,11 +1,10 @@
-package com.nagarro.pathway.controller;
+package com.nagarro.usermodule.controller;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- user-module/src/main/java/com/nagarro/usermodule/entity/User.java
+++ user-module/src/main/java/com/nagarro/usermodule/entity/User.java
@@ -1,6 +1,8 @@
 package com.nagarro.usermodule.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,8 +15,8 @@ import lombok.NoArgsConstructor;
 public class User {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
 	private String firstName;
 	private String lastName;
 	private String email;
