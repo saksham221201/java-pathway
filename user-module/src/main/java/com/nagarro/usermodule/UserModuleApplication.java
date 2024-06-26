4323a29c90fbfd96c02b523e416ec71cc031c45c
sakshamjoshi <sakshamjoshi1975@gmail.com>
2024-06-26 12:58:28 +0530

    Taking pull

--- user-module/src/main/java/com/nagarro/usermodule/UserModuleApplication.java
+++ user-module/src/main/java/com/nagarro/usermodule/UserModuleApplication.java
@@ -3,6 +3,7 @@ package com.nagarro.usermodule;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableEncryptableProperties
