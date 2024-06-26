1f58fbb5a968ca979c75a6539b315aa7d66906ca
sakshamjoshi <sakshamjoshi1975@gmail.com>
2024-06-26 13:30:30 +0530

    Modified Userservice added cachePut and cacheEvit

--- user-module/src/main/java/com/nagarro/usermodule/UserModuleApplication.java
+++ user-module/src/main/java/com/nagarro/usermodule/UserModuleApplication.java
@@ -3,7 +3,6 @@ package com.nagarro.usermodule;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableEncryptableProperties
