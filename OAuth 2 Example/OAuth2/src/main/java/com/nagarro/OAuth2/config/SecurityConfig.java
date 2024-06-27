3a50fd649593ac9ccd70c40872c8bd300634177c
kamadnayyar <kamad.nayyar@nagarro.com>
2024-06-27 12:39:02 +0530

    oauth2 logout

--- OAuth 2 Example/OAuth2/src/main/java/com/nagarro/OAuth2/config/SecurityConfig.java
+++ OAuth 2 Example/OAuth2/src/main/java/com/nagarro/OAuth2/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.nagarro.OAuth2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -17,11 +19,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((requests) -> requests
                         .anyRequest().permitAll()
                 )
-                .oauth2Login(oauthLogin ->
-                        oauthLogin
-                                .loginPage("/")
-                                .userInfoEndpoint(userInfoEndPoint -> userInfoEndPoint.oidcUserService(new OidcUserService())
-                                )
+                .oauth2Login(Customizer.withDefaults()
                 )
                 .logout(l-> l.logoutSuccessUrl("/").permitAll());
         return httpSecurity.build();
