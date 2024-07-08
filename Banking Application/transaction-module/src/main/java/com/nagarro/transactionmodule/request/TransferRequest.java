07cf915bf084fa1dc8cd92609e3c515ca43c5ab2
kamadnayyar <kamad.nayyar@nagarro.com>
2024-07-08 12:36:46 +0530

    money transfer implemented

---
+++ Banking Application/transaction-module/src/main/java/com/nagarro/transactionmodule/request/TransferRequest.java
@@ -0,0 +1,15 @@
+package com.nagarro.transactionmodule.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransferRequest {
+    private int debitAccountNumber;
+    private int creditAccountNumber;
+    private String email;
+    private double amount;
+}
--- Banking Application/transaction-module/src/main/java/com/nagarro/transactionmodule/service/TransactionService.java
+++ Banking Application/transaction-module/src/main/java/com/nagarro/transactionmodule/service/TransactionService.java
@@ -2,9 +2,12 @@ package com.nagarro.transactionmodule.service;
 
 import com.nagarro.transactionmodule.dto.AccountDTO;
 import com.nagarro.transactionmodule.request.TransactionRequest;
+import com.nagarro.transactionmodule.request.TransferRequest;
 
 public interface TransactionService {
     AccountDTO depositMoney(TransactionRequest transactionRequest);
 
     AccountDTO withdrawMoney(TransactionRequest transactionRequest);
+
+    AccountDTO transferMoney(TransferRequest transferRequest);
 }
