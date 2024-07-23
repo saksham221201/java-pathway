5b2910d6346dc8e263d786d8ac32b4b85bfe975a
sakshamjoshi <sakshamjoshi1975@gmail.com>
2024-07-23 12:11:23 +0530

    Uploaded document completed

--- Banking Application/loan-module/src/main/java/com/nagarro/loanmodule/dao/LoanDao.java
+++ Banking Application/loan-module/src/main/java/com/nagarro/loanmodule/dao/LoanDao.java
@@ -3,5 +3,8 @@ package com.nagarro.loanmodule.dao;
 import com.nagarro.loanmodule.entity.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LoanDao extends JpaRepository<Loan, String> {
+    Optional<Loan> findByAccountNumber(String accountNumber);
 }
--- Banking Application/loan-module/src/main/java/com/nagarro/loanmodule/service/LoanService.java
+++ Banking Application/loan-module/src/main/java/com/nagarro/loanmodule/service/LoanService.java
@@ -2,10 +2,12 @@ package com.nagarro.loanmodule.service;
 
 import com.nagarro.loanmodule.entity.Loan;
 import com.nagarro.loanmodule.request.VerifyStatusRequest;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface LoanService {
     Loan applyLoan(Loan loan);
     Loan verifyLoan(VerifyStatusRequest verifyStatusRequest);
-
-    
+    void storeDocument(MultipartFile multipartFile, String loanId) throws IOException;
 }
