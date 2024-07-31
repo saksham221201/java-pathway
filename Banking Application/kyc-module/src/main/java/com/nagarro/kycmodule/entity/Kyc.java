fd171b15be54e7eb228d3404559a307ff70a8b95
kamadnayyar <kamad.nayyar@nagarro.com>
2024-07-31 10:23:04 +0530

    kyc module security

--- Banking Application/kyc-module/src/main/java/com/nagarro/kycmodule/entity/Kyc.java
+++ Banking Application/kyc-module/src/main/java/com/nagarro/kycmodule/entity/Kyc.java
@@ -24,6 +24,7 @@ public class Kyc {
     @Column(name = "document", columnDefinition = "MEDIUMBLOB")
     private byte[] document;
 
+
     public String getImageDataBase64() {
         return Base64.encodeBase64String(this.document);
     }
