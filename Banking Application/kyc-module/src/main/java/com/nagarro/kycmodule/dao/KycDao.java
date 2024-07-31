d33614fcaa46740bbc6230dd2ce1d3ae7f3b50af
sakshamjoshi <sakshamjoshi1975@gmail.com>
2024-07-31 12:50:12 +0530

    Added Extract Text from PDF document

--- Banking Application/kyc-module/src/main/java/com/nagarro/kycmodule/dao/KycDao.java
+++ Banking Application/kyc-module/src/main/java/com/nagarro/kycmodule/dao/KycDao.java
@@ -3,5 +3,8 @@ package com.nagarro.kycmodule.dao;
 import com.nagarro.kycmodule.entity.Kyc;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface KycDao extends JpaRepository<Kyc,Integer> {
+    Optional<Kyc> findByEmail(String email);
 }
